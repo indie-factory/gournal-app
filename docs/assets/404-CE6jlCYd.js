f22d53a7653a5ff7ed285996a94babc5c5b53f34
IndieFactory <dev@indie-factory.com>
2024-04-29 20:14:52 +0900

    rebuild

---
+++ docs/assets/404-CE6jlCYd.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as t}from"./index-DGUKaLj6.js";const s={},n=t("h1",null,"404",-1),_=[n];function a(r,l){return c(),o("div",null,_)}const f=e(s,[["render",a]]);export{f as default};
